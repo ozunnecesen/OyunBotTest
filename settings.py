bc5e4182a4bd2a3e57890e7b6b328f3236ed4313
ozunnecesen <95986484+ozunnecesen@users.noreply.github.com>
2021-12-12 23:14:15 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
-TOKEN = "123456789876:Abkkanbsbaks"
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = getenv("BOT_TOKEN")
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
